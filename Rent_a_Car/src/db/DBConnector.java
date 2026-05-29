72d5f3ace73521023bb357145789882a1b08a305
naeun0825 <nayun0825@naver.com>
2026-05-29 21:29:40 +0900

    단순수정

--- Rent_a_Car/src/db/DBConnector.java
+++ Rent_a_Car/src/db/DBConnector.java
@@ -4,9 +4,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnector {
-	private static final String URL = "jdbc:mysql://127.0.0.1:3306/rental_car"; //localhost부분IP수정하셔서 사용하시면 됩니다  
+	private static final String URL = "jdbc:mysql://localhost:3306/rental_car"; //localhost부분IP수정하셔서 사용하시면 됩니다  
 	private static final String USER = "root"; //
- 	private static final String PASS = "qwerasdf12"; //여기에 각자 비밀번호 입력해서 사용하시면 됩니다
+ 	private static final String PASS = ""; //여기에 각자 비밀번호 입력해서 사용하시면 됩니다
 
 	public static Connection getConnection() throws SQLException {
 		return DriverManager.getConnection(URL, USER, PASS);
